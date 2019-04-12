e7d7cf53d828c616059419ef715f22dd9a6768b0
fcarlone <frankcarlone@gmail.com>
2019-04-12 17:49:57 -0400

    Set up app file struction and create GET request for concerts

---
+++ liri.js
@@ -0,0 +1,74 @@
+// Require and configure dotenv
+require('dotenv').config();
+const axios = require('axios');
+const moment = require('moment');
+// Import the keys.js file
+const keys = require('./keys');
+
+// let spotify = new Spotify(keys.spotify); **** Need to require 'node-spotify-api'
+
+// Spotify Key
+const spotifyKey = keys.spotify.id
+// Spotify Secrete
+const spotifySecrete = keys.spotify.secret
+// console.log(spotifyKey, spotifySecrete)
+
+// Store user input
+let userInput = process.argv;
+userInput.splice(0, 2)
+// console.log('userInput', userInput)
+const userCommand = userInput[0];
+const userSearch = userInput.slice(1).join(' ');
+console.log(userCommand);
+console.log(userSearch);
+
+
+// Switch statement for user commands
+switch (userCommand) {
+  case 'concert-this':
+    handleConcert();
+    break;
+  case 'spotify-this-song':
+    break;
+  case 'movie-this':
+    break;
+  case 'do-what-it-says':
+    break;
+  default:
+    console.log("Sorry.  Incorrect command.")
+}
+
+function handleConcert() {
+  console.log('handle concert invoked')
+  let url = `https://rest.bandsintown.com/artists/${userSearch}/events?app_id=codingbootcamp`;
+  console.log('url', url)
+
+  // Axios GET request
+  axios.get(url)
+    .then(function (response) {
+      // console.log('response', response.data[1])
+      let concertArray = response.data;
+      console.log('concertArray', concertArray)
+
+      if (concertArray.length === 0) {
+        console.log(`${userSearch.toUpperCase()} currently not touring`)
+      } else {
+
+        // loop through concert data array
+        console.log(`${userSearch.toUpperCase()} will be appearing at:`)
+        concertArray.forEach((data) => {
+          let concertVenueName = data.venue.name;
+          let concertVenueCity = data.venue.city;
+          let concertVenueCountry = data.venue.country;
+          let concertDateFormat = moment(data.datetime).format("dddd, MMMM Do YYYY, h:mm:ss a");
+          console.log(``)
+          console.log(`Venue: ${concertVenueName}`)
+          console.log(`Location: ${concertVenueCity}, ${concertVenueCountry}`)
+          console.log(`Date: ${concertDateFormat}`)
+          console.log(`==============================`)
+        })
+      }
+    }).catch(function (error) {
+      console.log('Error:', error);
+    });
+}
